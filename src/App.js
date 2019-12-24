42bad70e45cb4b40311636addec81de7b4888ce7
Su-Yeon Bak <bsuyeon16@gmail.com>
2019-12-24 16:20:28 +0900

    add notfound page and start implementing detail page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/common/Header';
 import List from './components/list/List';
+import Detail from './components/detail/Detail';
+import NotFound from './components/notFound/NotFound';
 import './index.css';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <List />
-    </div>
+    <BrowserRouter>
+      <div> 
+        <Header />
+
+        <Switch>
+          <Route path="/" component={List} exact />
+          <Route path="/currency/:id" component={Detail} exact/>
+          <Route component={NotFound}/>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 };
 
