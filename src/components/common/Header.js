3220e4c52f42ac583303fc61a3bd9af96c5df51e
Su-Yeon Bak <bsuyeon16@gmail.com>
2019-12-26 19:16:03 +0900

    Add Search components and Loading components props

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Search from './Search';
 import { Link } from 'react-router-dom';
 import logo from './logo.png';
 import './Header.css';
@@ -9,6 +10,8 @@ const Header = () => {
       <Link to="/">
         <img src={logo} alt="logo" className="Header-logo"/>
       </Link>
+
+      <Search />
     </div>
   );
 };
